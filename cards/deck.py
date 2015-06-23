6f180209c788e86e880b6a7f137be99a3af9fbc3
Hestiaflame <mayam@girlsms.org>
2015-06-22 19:43:15 -0700

    Fixed some stuff
    Based on cproctor's feedback

--- cards/deck.py
+++ cards/deck.py
@@ -10,60 +10,41 @@ class Deck():
 
 
 # This saves the attributes of deck for later.
-    	def __init__(self):
-		self.cards = [Card(card_num) for card_num in range(52)]
-		self.discard_pile = []
-		self.num_cards = 52
+    	def __init__(self, cards=[Card(card_num) for card_num in range(52)]):
+		self.cards = cards
 
 # This shows what the class is as specifically as possible. Not meant to be turned into a string.
 	def __repr__(self):
-		return "<Deck with {} cards>".format(self.num_cards)
-# Shuffles the deck
+		return "<Deck with {} cards>".format(self.count())
+
+# Shuffle deck
 	def mix(self):
 		shuffle(self.cards)
 
-# Draws a card
+# Count the number of cards in the deck
+	def count(self):
+		return len(self.cards)
+
+# Draw a card
 	def draw(self):
-		if self.num_cards == 0:
+		if self.count() == 0:
 			return None
 		else:
-			last_card = self.cards.pop()
-			self.discard_pile.append(last_card)
-			self.num_cards -= 1
-			return last_card
+			return self.cards.pop()
 
-# Shows the top card in the deck
+# Look at top card
 	def peek(self):
 		return self.cards[len(self.cards) - 1]
 
-# Tells you how many cards are in the deck
-	def count(self):
-# len(self.cards) should equal num_cards - you can replace it if you would like.
-		return len(self.cards)
 
-# Checks if the deck is empty
+# Check if the deck is empty
 	def empty(self):
-		if self.num_cards == 0:
+		if count() == 0:
 			return True
 		else:
 			return False
 
-# Adds a card
+# Add a card
 	def add(self, card):
 		self.cards.append(card)
-		self.num_cards += 1
-
-# Empties the deck
-	def finish(self):
-		for each_card in self.cards:
-			self.discard_pile.append(each_card)
-		self.cards = []
-		self.num_cards = 0
-
-# Refills the deck
-	def refill(self):
-		for each_card in self.discard_pile:
-			self.cards.append(each_card)
-		self.discard_pile = []
-		self.num_cards = 52
 
